b801cc25cb40d3aeadb33111aeddd563b55b3313
Zhu Wenbo <daniel.wb.zhu@gmail.com>
2025-09-12 21:05:57 +0800

    Revert "Add Complete Exception Handling"
    This reverts commit b0ff5dfc4548e1c3a757657c9b208a03ed3e0b29.

--- src/main/java/exceptions/InvalidFormatException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class InvalidFormatException extends RobonautException {
-    public InvalidFormatException(String message) {
-        super("OOPS!!! " + message);
-    }
-}
--- src/main/java/exceptions/UnknownCommandException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class UnknownCommandException extends RobonautException {
-    public UnknownCommandException(String command) {
-        super("OOPS!!! I'm sorry, but I don't know what '" + command + "' means :-(");
-    }
-}
